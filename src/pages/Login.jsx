be011222c25cdc8130944266c9fd39fb80262cca
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2024-12-06 00:58:21 +0530

    Initial setup done

---
+++ src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+const Login = () => {
+  return <main></main>;
+};
+
+export default Login;
